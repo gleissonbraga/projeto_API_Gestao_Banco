b9b565997dd62210c223e78fbbf4bcf4d5bb2dfc
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2024-10-20 18:31:13 -0300

    feat: adicionado as funcionalidades de criar, inserir e deletar

--- app.js
+++ app.js
@@ -12,9 +12,28 @@ app.get('/', (req, res) => {
 })
 
 app.get('/mostrar', (req, res) => {
-    res.json(user_repository.showUsers)
+    res.json(user_repository.showUsers())
 })
-app.post('/criar', user_repository.createdUser)
+app.post('/criar', (req, res) => {
+    const user = req.body
+    res.json(user_repository.createdUser(user))
+})
+
+app.get('/buscar/:id', (req, res) => {
+    const id = req.params.id
+    res.json(user_repository.findUser(id))
+})
+
+app.post('/deletar/:id', (req, res) => {
+    const id = req.params.id
+    res.json(user_repository.deleteUser(id))
+})
+
+app.post('/atualizar/:id', (req, res) => {
+    const id = req.params.id
+    const user = req.body
+    res.json(user_repository.updateUser(user, id))
+})
 
 
 app.listen(PORT, () => {
--- repository/user_repository.js
+++ repository/user_repository.js
@@ -1,5 +1,5 @@
 let listUser = []
-let idGenerator = []
+let idGenerator = 1
 
 
 function showUsers(){
@@ -10,18 +10,35 @@ function createdUser(user){
     if(!user.name || !user.cpf || !user.password) {
         return
     }
-
     user.id = idGenerator++
     listUser.push(user)
     return user
 }
 
 function findUser(id){
-    return (listUser.find(user => {
-        user.id == id
-    }))
+    return listUser.find(user => user.id == id)
+}
+
+function updateUser(user, id) {
+    if(!user.name, user.cpf, user.password) {
+        return
+    }
+
+    let indiceUser = listUser.findIndex(user => user.id == id)
+    
+    if(indiceUser == -1) return
+
+    user.id = id
+    listUser[indiceUser] = user
+    return user
+}
+
+function deleteUser(id) {
+    const indiceUser = listUser.findIndex(user => user.id == id)
+    if(indiceUser == -1) return
+    return (listUser.splice(indiceUser, 1))[0];
 }
 
 module.exports = {
-   showUsers, createdUser, findUser
+   showUsers, createdUser, findUser, deleteUser, updateUser
 }
