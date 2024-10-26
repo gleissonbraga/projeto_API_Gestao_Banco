f5a073f33c8165e7e33da3112da671816ac96405
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2024-10-26 20:09:15 -0300

    feat: inserindo a fiuncionalidade de busca por cupom especifico e todods os cupons

--- repository/transaction_repository.js
+++ repository/transaction_repository.js
@@ -5,21 +5,9 @@ let listTransactionsWithdrawal = []
 let listTransactionsPix = []
 
 
-function showTransactionDeposit(){
-    return listTransactionsDeposit
-}
-
-function showTransactionWithdrawal(){
-    return listTransactionsWithdrawal
-}
-
-function showTransactionPix(){
-    return listTransactionsPix
-}
-
-
 function createdDepositReceipt(account, value){
     listTransactionsDeposit.push({
+        transaction: "Depósito",
         name: account.name,
         cpf: account.cpf,
         account: account.account,
@@ -31,6 +19,7 @@ function createdDepositReceipt(account, value){
 
 function createdWithdrawalReceipt(account, value){
     listTransactionsWithdrawal.push({
+        transaction: "Saque",
         name: account.name,
         cpf: account.cpf,
         account: account.account,
@@ -42,6 +31,7 @@ function createdWithdrawalReceipt(account, value){
 
 function createdDepositPix(account, value) {
     listTransactionsPix.push({
+        transaction: "Pix",
         name: account.name,
         cpf: account.cpf,
         account: account.account,
@@ -50,6 +40,70 @@ function createdDepositPix(account, value) {
         hours: format(new Date(), 'HH:mm')
     })
 }
+
+function findATransactionDeposit(account, date){
+    if(!account || !date) return
+    const findAccount = listTransactionsDeposit.findIndex(dep => dep.account == account)
+    const findDate = listTransactionsDeposit.findIndex(dep => dep.transactionDate == date)
+    if(findAccount == undefined || findDate == undefined) {
+        return undefined
+    } else {
+        if(findAccount !== -1 && findDate !== -1 && findAccount === findDate){
+            return listTransactionsDeposit[findAccount]
+        }
+    }
+}
+
+function findATransactionWithdraw(account, date){
+    if(!account || !date) return
+    const findAccount = listTransactionsWithdrawal.findIndex(dep => dep.account == account)
+    const findDate = listTransactionsWithdrawal.findIndex(dep => dep.transactionDate == date)
+    if(findAccount == undefined || findDate == undefined) {
+        return undefined
+    } else {
+        if(findAccount !== -1 && findDate !== -1 && findAccount === findDate){
+            return listTransactionsWithdrawal[findAccount]
+        }
+    }
+}
+
+function findATransactionPix(account, date){
+    if(!account || !date) return
+    const findAccount = listTransactionsPix.findIndex(dep => dep.account == account)
+    const findDate = listTransactionsPix.findIndex(dep => dep.transactionDate == date)
+    if(findAccount == undefined || findDate == undefined) {
+        return undefined
+    } else {
+        if(findAccount !== -1 && findDate !== -1 && findAccount === findDate){
+            return listTransactionsPix[findAccount]
+        }
+    }
+}
+
+function findAllTransactionDeposit(account){
+    if(!account) return
+    const findAccount = listTransactionsDeposit.filter(dep => dep.account === account)
+    if(findAccount) {
+        return findAccount
+    }
+}
+
+function findAllTransactionWithdraw(account){
+    if(!account) return
+    const findAccount = listTransactionsWithdrawal.filter(dep => dep.account === account)
+    if(findAccount) {
+        return findAccount
+    }
+}
+
+function findAllTransactionPix(account){
+    if(!account) return
+    const findAccount = listTransactionsPix.filter(dep => dep.account === account)
+    if(findAccount) {
+        return findAccount
+    }
+}
+
 
 
 
